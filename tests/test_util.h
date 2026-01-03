2751b871a589bca4442154523870775975c8d891
fanyang <fanyang89@outlook.com>
2026-01-03 15:28:52 +0800

    Rewrite doctest_p macro

--- tests/test_util.h
+++ tests/test_util.h
@@ -2,13 +2,13 @@
 
 #include <doctest/doctest.h>
 
-#define DOCTEST_VALUE_PARAMETERIZED_DATA(data, data_container)                                                       \
-    static size_t _doctest_subcase_idx = 0;                                                                          \
-    std::for_each(data_container.begin(), data_container.end(), [&](const auto& in) {                                \
-        DOCTEST_SUBCASE((std::string(#data_container "[") + std::to_string(_doctest_subcase_idx++) + "]").c_str()) { \
-            data = in;                                                                                               \
-        }                                                                                                            \
-    });                                                                                                              \
-    _doctest_subcase_idx = 0
+#define DOCTEST_VALUE_PARAMETERIZED_DATA(data, data_container)                                        \
+    if (auto i = 0; true) {                                                                           \
+        for (auto it = data_container.begin(); it != data_container.end(); ++it) {                    \
+            DOCTEST_SUBCASE((std::string(#data_container "[") + std::to_string(i++) + "]").c_str()) { \
+                data = *it;                                                                           \
+            }                                                                                         \
+        }                                                                                             \
+    }
 
 namespace raftpp {}
